be0022e713a97ae9a2d4b77adce88414043a43f1
Esai Morales <esai@Esais-iMac.local>
2017-10-25 22:10:18 -0700

    populating course table

---
+++ Database/populate.sql
@@ -0,0 +1,17 @@
+INSERT INTO course (school, department, course_number)
+    VALUES('San Jose State Univeristy', 'CSE', '101'),
+    VALUES('San Jose State Univeristy', 'CSE', '102'),
+    VALUES('San Jose State Univeristy', 'CSE', '103'),
+    VALUES('San Jose State Univeristy', 'CSE', '104'),
+    VALUES('Stanford Univeristy', 'CS', '101'),
+    VALUES('Stanford Univeristy', 'CS', '102'),
+    VALUES('Stanford Univeristy', 'CS', '103'),
+    VALUES('Stanford Univeristy', 'CS', '104'),
+    VALUES('Univeristy of California, Santa Cruz', 'CSE', '201'),
+    VALUES('Univeristy of California, Santa Cruz', 'CSE', '202'),
+    VALUES('Univeristy of California, Santa Cruz', 'CSE', '203'),
+    VALUES('Univeristy of California, Santa Cruz', 'CSE', '204'),
+    VALUES('Santa Clara Univeristy', 'COEN', '201'),
+    VALUES('Santa Clara Univeristy', 'COEN', '202'),
+    VALUES('Santa Clara Univeristy', 'COEN', '203'),
+    VALUES('Santa Clara Univeristy', 'COEN', '204');
