ace9bf776ef80d7cbb6bfdd69aa773ef7b51ec03
Kevin Velcich <kvelcich@gmail.com>
2017-10-26 02:05:55 -0700

    adding more populating data

--- Database/populate.sql
+++ Database/populate.sql
@@ -11,7 +11,27 @@ INSERT INTO course (school, department, course_number)
     ('Univeristy of California, Santa Cruz', 'CSE', '202'),
     ('Univeristy of California, Santa Cruz', 'CSE', '203'),
     ('Univeristy of California, Santa Cruz', 'CSE', '204'),
-    ('Santa Clara Univeristy', 'COEN', '201'),
-    ('Santa Clara Univeristy', 'COEN', '202'),
-    ('Santa Clara Univeristy', 'COEN', '203'),
-    ('Santa Clara Univeristy', 'COEN', '204');
+    ('Santa Clara University', 'COEN', '201'),
+    ('Santa Clara University', 'COEN', '202'),
+    ('Santa Clara University', 'COEN', '203'),
+    ('Santa Clara University', 'COEN', '204');
+
+INSERT INTO equivalent (internal_id, external_id, is_equivalent)
+SELECT (SELECT C.course_id from course C where C.school = 'Santa Clara University' AND C.department = 'COEN' AND C.course_number = '201'),
+(SELECT C.course_id from course C where C.school = 'San Jose State Univeristy' AND C.department = 'CSE' AND C.course_number = '102'),
+1;
+
+INSERT INTO equivalent (internal_id, external_id, is_equivalent)
+SELECT (SELECT C.course_id from course C where C.school = 'Santa Clara University' AND C.department = 'COEN' AND C.course_number = '202'),
+(SELECT C.course_id from course C where C.school = 'Univeristy of California, Santa Cruz' AND C.department = 'CSE' AND C.course_number = '202'),
+1;
+
+INSERT INTO equivalent (internal_id, external_id, is_equivalent)
+SELECT (SELECT C.course_id from course C where C.school = 'Santa Clara University' AND C.department = 'COEN' AND C.course_number = '202'),
+(SELECT C.course_id from course C where C.school = 'Stanford Univeristy' AND C.department = 'CS' AND C.course_number = '104'),
+1;
+
+INSERT INTO equivalent (internal_id, external_id, is_equivalent)
+SELECT (SELECT C.course_id from course C where C.school = 'Santa Clara University' AND C.department = 'COEN' AND C.course_number = '204'),
+(SELECT C.course_id from course C where C.school = 'San Jose State Univeristy' AND C.department = 'CSE' AND C.course_number = '101'),
+0;
