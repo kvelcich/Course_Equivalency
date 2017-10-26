24477395fc8d23f79694ebc9d77d2da8d41162c2
Kevin Velcich <kvelcich@gmail.com>
2017-10-25 22:24:48 -0700

    Changing DB schema

--- Database/createdb.sql
+++ Database/createdb.sql
@@ -1,22 +1,28 @@
 use sdb_kvelcich;
 
 CREATE TABLE course (
-    course_id       INT NOT NULL AUTO_INCREMENT,
-    school    		VARCHAR(50),
-    department    	VARCHAR(50),
-    course_number   VARCHAR(10),
-    PRIMARY KEY (course_id)
+    school    				VARCHAR(50),
+    department    			VARCHAR(50),
+    course_number   		VARCHAR(10),
+    PRIMARY KEY (school, department, course_number)
 );
 
 CREATE TABLE equivalent (
-    internal_id      INT NOT NULL,
-    external_id      INT NOT NULL,
-    is_equivalent    INT(1),
+	internal_school			VARCHAR(50),
+	internal_department		VARCHAR(50),
+	internal_course_number	VARCHAR(10),
+	external_school			VARCHAR(50),
+	external_department		VARCHAR(50),
+	external_course_number	VARCHAR(10),
+    is_equivalent    		INT(1),
 
-    FOREIGN KEY (internal_id) REFERENCES course(course_id),
-    FOREIGN KEY (external_id) REFERENCES course(course_id),
+    FOREIGN KEY (internal_school, internal_department, internal_course_number)
+	REFERENCES course(school, department, course_number),
 
-    CHECK (internal_id != external_id),
+	FOREIGN KEY (external_school, external_department, external_course_number)
+	REFERENCES course(school, department, course_number),
+
+	CHECK (internal_school = 'Santa Clara University'),
     CHECK (is_equivalent = 1 OR is_equivalent = 0)
 );
 
