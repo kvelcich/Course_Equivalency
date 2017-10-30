a3e16b832ebd3c29f6b4114f6951f54a884f495d
Kevin Velcich <kvelcich@gmail.com>
2017-10-30 15:09:40 -0700

    Database Integrations (#2)
    * fixing functionalities
    
    * fixing vc
    
    * adding search functionality

--- Application/script.js
+++ Application/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$('#login_button').click(function(){
+	$('#login_button').click(function() {
 		var function_name = 'login';
 		var email = $('input#email_input').val();
 		var pass = $('input#pass_input').val();
@@ -7,52 +7,40 @@ $(document).ready(function(){
 		$.post('ajax.php', {function: function_name, email: email, password: pass}, function(data) {
 			if (data == 1) {
 				console.log('Login successful');
-				window.location.href = 'search.html';
+        window.location.href = 'search.html';
 			} else if (data == 0) {
 				console.log('Invalid login');
-				alert('Invalid Login Info!');
+        alert('Invalid Login Info!');
 			}
 		});
-    });
+  });
 
-	$('select[id="schoolSelect"]').change(function() {
+	$('select[id="school_select"]').change(function() {
 		var function_name = 'get_departments';
-		var school_name = $(this).val();
+		var school_name = $('select[id="school_select"] option:selected').text();
 
 		$.post('ajax.php', {function: function_name, school: school_name}, function(data) {
+			listDepartments(JSON.parse(data));
+		});
+	});
+
+	$('select[id="department_select"]').change(function() {
+		var function_name = 'get_schools';
+		var department_name = $('select[id="department_select"] option:selected').text();
+
+		$.post('ajax.php', {function: function_name, department: department_name}, function(data) {
+			listSchools(JSON.parse(data));
+		});
+	});
+
+	$('#search').click(function() {
+		var function_name = 'search';
+		var school_name = $('select[id="school_select"] option:selected').text();
+		var department_name = $('select[id="department_select"] option:selected').text();
+		var course_number = $('input#number_select').val();
+
+		$.post('ajax.php', {function: function_name, school: school_name, department: department_name, number: course_number}, function(data) {
 			$('div#advdiv').append(data);
 		});
 	});
-
-/* OUTDATED:
-	$('#name-submit').click(function(){
-		var function_name = 'get_schools';
-		var department_name = '';
-		$.post('ajax.php', {function: function_name, department: department_name}, function(data) {
-			$('div#schools').append(data);
-			//console.log(JSON.parse(data).length);
-		});
-    });
-
-	$('#department').click(function(){
-		var function_name = 'get_departments';
-		var school_name = 'San Jose State Univeristy';
-		$.post('ajax.php', {function: function_name, school: school_name}, function(data) {
-			$('div#departments').append(data);
-		});
-	});
-
-	$('#advisor').click(function() {
-		var function_name = 'add_user';
-		var email = 'hunter@gmail.com';
-		var pass = 'sample';
-		$.post('ajax.php', {function: function_name, email: email, password: pass}, function(data) {
-				if (data == 1) {
-					$('div#advdiv').append('Success');
-				} else if (data == 0) {
-					$('div#advdiv').append('Error, username already in use');
-				}
-		});
-	});
-*/
 });
