5c280941004dd2bbf0ed7d8a7f9918a68bfb78be
Kevin Velcich <kvelcich@gmail.com>
2017-10-30 10:19:52 -0700

    login support

--- Application/script.js
+++ Application/script.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
-	$('#name-submit').click(function(){
-		var function_name = 'get_schools';
-		var department_name = '';
-		$.post('ajax.php', {function: function_name, department: department_name}, function(data) {
-			$('div#schools').append(data);
-			//console.log(JSON.parse(data).length);
+	$('#login_button').click(function(){
+		var function_name = 'login';
+		var email = $('input#email_input').val();
+		var pass = $('input#pass_input').val();
+
+		$.post('ajax.php', {function: function_name, email: email, password: pass}, function(data) {
+			if (data == 1) {
+				console.log('Login successful');
+			} else if (data == 0) {
+				console.log('Invalid login');
+			}
 		});
     });
 
