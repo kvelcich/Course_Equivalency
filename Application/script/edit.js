675a603a67f0c4d30d3edf6ea4f724a74c2cfccf
Esai Morales <esaimn96@gmail.com>
2017-10-31 03:08:20 -0700

    edit populate inputs

--- Application/script/edit.js
+++ Application/script/edit.js
@@ -1,3 +1,25 @@
 function populateInputs(equivalencyData){
-    
+    var obj = equivalencyData[0];
+    var is_equivalent = 'No';
+    var other_option = 'Yes';
+    if (obj.is_equivalent == 1) {
+        is_equivalent = 'Yes';
+        other_option = 'No';
+    }
+
+    document.getElementById('school_external').value = obj.school_external;
+    document.getElementById('department_external').value = obj.department_external;
+    document.getElementById('number_external').value = obj.number_external;
+    document.getElementById('department_internal').value = obj.department_internal;
+    document.getElementById('number_internal').value = obj.number_internal;
+
+    var select = document.getElementById('isEquivalentSelect');
+    var option = document.createElement("option");
+    option.text = is_equivalent;
+    option.setAttribute('selected', 'selected');
+    select.appendChild(option);
+
+    var option2 = document.createElement('option');
+    option2.text = other_option;
+    select.appendChild(option2);
 }
