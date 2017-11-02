ae4c870055f02934c38f9cd8a991e883a7e70044
tracys9522 <gsun@scu.edu>
2017-11-02 13:26:47 -0700

    Update searchschool.php

--- Application/searchschool.php
+++ Application/searchschool.php
@@ -1,11 +1,12 @@
 <?php
 require 'database.php';
-  $connect = connect();
+  
   //get search term
   $searchTerm = $_GET['term'];
 
+  $connect = connect();
   //get matched data from schools
-  $query = "SELECT * FROM course WHERE school LIKE '%".$searchTerm."%' ORDER BY school";
+  $query = "SELECT DISTINCT school FROM course WHERE school LIKE '%".$searchTerm."%' ORDER BY school";
   $result = $connect->query($query);
 
   while($row = mysqli_fetch_assoc($result)) {
