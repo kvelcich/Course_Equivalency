a5c0e4790bb0ce200c3b7ce2f8335baf0e7b19c3
tracys9522 <gsun@scu.edu>
2017-11-02 14:10:27 -0700

    Update searchdep.php

--- Application/searchdep.php
+++ Application/searchdep.php
@@ -1,19 +1,19 @@
 <?php
 require 'database.php';
+  $connect = connect();
+
   //get search term
   $searchTerm = $_GET['term'];
 
-  $connect = connect();
-
   //select data from department
-  $query = "SELECT DINSTINCT department FROM course WHERE department LIKE '%".$searchTerm."%' ORDER BY department";
+  $query = "SELECT DINSTINCT department FROM course WHERE department LIKE '%$searchTerm%' ORDER BY department";
 
   $result = $connect->query($query);
 
   while($row = mysqli_fetch_assoc($result)) {
       $rows[] = $row['department'];
   }
+  $connect -> close();
   //return json data
   echo json_encode($rows);
-  $connect -> close();
 ?>
