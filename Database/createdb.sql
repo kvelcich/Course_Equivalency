64792afc1bdb7c94d5f457dfc3ca2b5628ba5aa9
Kevin Velcich <kvelcich@gmail.com>
2017-11-13 20:02:11 -0800

    changing db

--- Database/createdb.sql
+++ Database/createdb.sql
@@ -1,29 +1,32 @@
-  use sdb_kvelcich;
+use sdb_kvelcich;
 
 CREATE TABLE course (
     course_id       INT NOT NULL AUTO_INCREMENT,
-    school    		VARCHAR(50),
+    school    		  VARCHAR(500),
     department    	VARCHAR(50),
     course_number   VARCHAR(10),
     PRIMARY KEY (course_id)
 );
 
 CREATE TABLE equivalent (
-    internal_id      INT NOT NULL,
-    external_id      INT NOT NULL,
-    is_equivalent    INT(1),
+    internal_id       INT NOT NULL,
+    external_id       INT NOT NULL,
+    is_equivalent     INT(1),
+    evaluator         VARCHAR(100) NOT NUll,
+    reason            TEXT,
 
-	PRIMARY KEY (internal_id, external_id),
-    FOREIGN KEY (internal_id) REFERENCES course(course_id),
-    FOREIGN KEY (external_id) REFERENCES course(course_id),
+    PRIMARY KEY (internal_id, external_id),
+    FOREIGN KEY (internal_id) REFERENCES course (course_id),
+    FOREIGN KEY (external_id) REFERENCES course (course_id),
+    FOREIGN KEY (evaluator) REFERENCES advisor (email),
 
     CHECK (internal_id != external_id),
     CHECK (is_equivalent = 1 OR is_equivalent = 0)
 );
 
 CREATE TABLE advisor (
-    email           VARCHAR(25) NOT NULL,
-    password        VARCHAR(50),
+    email           VARCHAR(100) NOT NULL,
+    password        VARCHAR(100),
 
     PRIMARY KEY (email)
 );
