8d75adea4f59b3896ecdc4d7d1dbcb3bf3de327d
Kevin Velcich <kvelcich@gmail.com>
2017-11-27 15:13:44 -0800

    adding config json to read database information

--- Application/database.php
+++ Application/database.php
@@ -3,15 +3,15 @@
 				$string = file_get_contents("assets/config.json");
 				$config_json = json_decode($string, true);
 
-echo $json_a['John'][status];
+echo $config_json['John'][status];
 echo $json_a['Jennifer'][status];
 		ini_set('display_errors','On');
 		error_reporting(E_ALL);
 
-		$db_host = "dbserver.engr.scu.edu";
-		$db_user = "kvelcich";
-		$db_pass = "00001106223";
-		$db_name = "sdb_kvelcich";
+		$db_host =  $config_json['database_host'];
+		$db_user = $config_json['database_user'];
+		$db_pass = $config_json['database_password'];
+		$db_name = $config_json['database_name'];
 
 		$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name)
 				or die("Error" . mysqli_error($conn));
