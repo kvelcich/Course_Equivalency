6cdd05ad865ccf50d4f63c923f487a9e9187d628
Kevin Velcich <kvelcich@gmail.com>
2017-11-27 20:21:45 -0800

    Delete session.js

--- Application/script/session.js
+++
@@ -1,8 +0,0 @@
-var function_name = 'start_session';
-$.post('session.php', {function: function_name}, function(data) {
-});
-
-var function_name = 'get_username';
-$.post('session.php', {function: function_name}, function(data) {
-		if (data == 'username - invalid') window.location.href = 'login.html';
-});
