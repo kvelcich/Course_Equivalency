330316f99f20a048ddcc076d43c93b39b9f9cdd2
gsun <gsun@scu.edu>
2017-11-27 16:35:10 -0800

    changes

--- Application/script/add.js
+++ Application/script/add.js
@@ -11,12 +11,6 @@ $(document).ready(function(){
 		});
 	});
 	$('#add_button').click(function() {
-		/*$('#dep_internal').prop('required',true);
-		$('#num_internal').prop('required',true);
-		$('#school_external').prop('required',true);
-		$('#dep_external').prop('required',true);
-		$('#num_external').prop('required',true);
-		$('#reason').prop('required',true);
 		// Check if all entries filled out
 		var function_name = 'add_entry';
 		var dep_internal = $('input#dep_internal').val();
@@ -26,15 +20,7 @@ $(document).ready(function(){
 		var num_external = $('input#num_external').val();
 		var equiv = $('select[id="equivalency"] option:selected').text();
 		var reason = $('input#reason').val();
-*/
-		var function_name = 'add_entry';
-		var dep_internal = document.getElementById("dep_internal");
-		var num_internal = document.getElementById("num_internal");
-		var school_external = document.getElementById("school_external");
-		var dep_external = document.getElementById("dep_external");
-		var num_external = document.getElementById("num_external");
-		var equiv = $('select[id="equivalency"] option:selected').text();
-		var reason = document.getElementById("reason");
+
 		$.post('session.php', {function: 'get_username'}, function(username) {
 				$.post('ajax.php', {
 						function: function_name,
