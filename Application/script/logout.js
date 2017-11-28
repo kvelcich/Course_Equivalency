70b6e0b4c66dd74982d30ab69f7d2eb399e6ea7b
Kevin Velcich <kvelcich@gmail.com>
2017-11-27 20:21:34 -0800

    Delete logout.js

--- Application/script/logout.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('div.logout').click(function(){
-        var function_name = 'logout';
-
-        $.post('session.php', {function: function_name}, function(data) {
-            window.location.href = 'login.html';
-        });
-    });
-});
