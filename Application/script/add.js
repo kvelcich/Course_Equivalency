42ef526d43f27f9515ea4ab3883c83794187e3a6
gsun <gsun@scu.edu>
2017-11-27 20:03:34 -0800

    commenting

--- Application/script/add.js
+++ Application/script/add.js
@@ -1,3 +1,4 @@
+//validate empty textbox
 function validateTextBox(val1,val2,val3,val4,val5,val6) {
     if (val1== "" || val2=="" || val3=="" || val4=="" || val5=="" || val6==""){
         alert("Incomplete Information!");
@@ -8,7 +9,7 @@ function validateTextBox(val1,val2,val3,val4,val5,val6) {
 
 $(document).ready(function(){
 	$(function() {
-
+    //autocomplete textboxes
 		$("#school_external").autocomplete({
 			source:'searchschool.php',
 			minLength: 1
@@ -17,9 +18,14 @@ $(document).ready(function(){
 			source:'searchdep.php',
 			minLength: 1
 		});
+    $("#dep_internal").autocomplete({
+      source:'searchdep.php',
+      minLength: 1
+    });
 	});
-	$('#add_button').click(function() {
 
+  //onclick button function
+	$('#add_button').click(function() {
 		var function_name = 'add_entry';
 		var dep_internal = $('input#dep_internal').val();
 		var num_internal = $('input#num_internal').val();
@@ -29,10 +35,11 @@ $(document).ready(function(){
 		var equiv = $('select[id="equivalency"] option:selected').text();
 		var reason = $('input#reason').val();
 
+    //validate empty textboxes
 		var valid = validateTextBox(dep_internal,num_internal,school_external,dep_external,num_external,reason);
     if (valid == true){
-		$.post('session.php', {function: 'get_username'}, function(username) {
-				$.post('ajax.php', {
+      $.post('session.php', {function: 'get_username'}, function(username) {
+        $.post('ajax.php', {
 						function: function_name,
 						school_external: school_external,
 						department_external: dep_external,
@@ -46,7 +53,7 @@ $(document).ready(function(){
 						if (data < 0) alert('Error Adding Equivalency!\nPlease check equivalency does not already exist');
 						else if (data == 1) window.location.href = 'search.html';
 				});
-		});
+      });
   }
-	});
+});
 });
